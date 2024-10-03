3fdeb5d71e24429d83395bcf6e05380b78c4fc56
Yvan <yvandouis@hotmail.fr>
2024-10-03 13:54:18 +0200

    var this

--- interaction.js
+++ interaction.js
@@ -3,7 +3,10 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-
+    this.initPosDnD_x = 0;
+    this.initPosDnD_y = 0;
+    this.finalPosDnD_x = 0;
+    this.finalPosDnD_y = 0;
 	// Developper les 3 fonctions gérant les événements
 
 	// Associer les fonctions précédentes aux évènements du canvas.
