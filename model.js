08f17cc28886ae6acc5169bc5f0cb4dd9e2e2932
Yvan <yvandouis@hotmail.fr>
2024-10-03 14:34:04 +0200

    Q3

--- model.js
+++ model.js
@@ -1,3 +1,29 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function drawing(){
+    this.formesArr = new Array();
+
+    this.addForme = function(forme){
+        this.formesArr.push(forme);
+    }
+}
